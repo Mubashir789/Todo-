8f2a5bf7fc52dea786b95fa26e949d1a0db4e064
Mubashir <mubashir37890@gmail.com>
2022-10-04 17:20:36 +0500

    code

--- server.mjs
+++ server.mjs
@@ -1,36 +1,77 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose';
+
+
+let todoSchema = new mongoose.Schema({
+    text: { type: String, required: true },
+    Id: String,
+    createOn: { type: Date, default: Date.now },
+});
+
+const todoModel = mongoose.model('todos', todoSchema);
+
 
 
 const app = express()
 const port = process.env.PORT || 3000
 
-let todos = [];
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 
 app.post('/todo', (req, res) => {
 
-    todos.push(req.body.text);
 
-    res.send({
 
-        message: "Your todo is Saved",
-        data: todos
+    todoModel.create({ text: req.body.text }, (err, saved) => {
+        if (!err) {
 
+            console.log(saved)
+
+            res.send({
+
+                message: "Your todo is Saved",
+
+            })
+        } else {
+            res.status(500).send({
+
+                message: "Server Error!"
+
+            })
+        }
     })
+
+
 })
 
 app.get('/todos', (req, res) => {
 
-    res.send({
+    todoModel.find({}, (err, data) => {
+        if (!err) {
+            res.send({
 
-        message: "Here is your todo list",
-        data: todos
+                message: "Here is your todo list",
+                data: data
 
-    })
+            })
+
+        }else {
+            res.status(500),send({
+
+                message: "Server Error!"
+
+            })
+
+        }
+
+
+    });
+
+
+
 })
 
 
@@ -39,3 +80,36 @@ app.get('/todos', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+let dbURI = 'mongodb+srv://username:username@cluster0.640ylgv.mongodb.net/First-Database?retryWrites=true&w=majority';
+mongoose.connect(dbURI);
+
+
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function () {//connected
+    console.log("Mongoose is connected");
+    // process.exit(1);
+});
+
+mongoose.connection.on('disconnected', function () {//disconnected
+    console.log("Mongoose is disconnected");
+    process.exit(1);
+});
+
+mongoose.connection.on('error', function (err) {//any error
+    console.log('Mongoose connection error: ', err);
+    process.exit(1);
+});
+
+process.on('SIGINT', function () {/////this function will run jst before app is closing
+    console.log("app is terminating");
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection closed');
+        process.exit(0);
+    });
+});
